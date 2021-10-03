dd7843363fee047dabb8da2563c17cdf26d5615b
Vlad Basoon <vbasoon@gmail.com>
2021-10-03 10:40:36 +0300

    header ready

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,12 @@
+let menuBtn = document.querySelector("#menu-btn");
+let menuNav = document.querySelector(".header__menu");
 
+menuBtn.onclick = () => {
+  menuBtn.classList.toggle("fa-times");
+  menuNav.classList.toggle("active");
+};
+
+menu.onscroll = () => {
+  menu.classList.remove("fa-times");
+  navbar.classList.remove("active");
+};
